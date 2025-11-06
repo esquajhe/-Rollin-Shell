ee23d3ea1a659292313cab07ac2416bae41bae2e
esquajhe <fusiljulian@gmail.com>
2025-11-06 19:14:03 +0100

    Mise à jour

--- Rolling Shell/Assets/PlayerMovement.cs
+++ Rolling Shell/Assets/PlayerMovement.cs
@@ -11,9 +11,23 @@ public class PlayerMovement : MonoBehaviour
     public LayerMask GroundMask;
 
     float x, y;
-    bool sprint;
-    bool jump;
+    bool sprint = false;
+    bool jump = false;
     Rigidbody rb;
+
+    const int MAX_NB_JUMP = 2;
+    int current_nb_jump = 0;
+
+    bool IsGrounded()
+    {
+        float GroundedDistance = 0.5f;
+        rb = GetComponent<Rigidbody>();
+        if (rb.velocity.y == 0)
+        {
+            return Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, GroundedDistance);
+        }
+        return false;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -26,8 +40,13 @@ public class PlayerMovement : MonoBehaviour
     {
         x = Input.GetAxisRaw("Horizontal");
         y = Input.GetAxisRaw("Vertical");
+        sprint = Input.GetKeyDown(KeyCode.LeftShift);
 
-        sprint = Input.GetButton("Sprint");
+        if (Input.GetButton("Sprint"))
+        {
+            sprint = true;
+        }
+
         if (Input.GetButtonDown("Jump"))
             jump = true;
     }
@@ -35,12 +54,17 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         rb.velocity = new Vector3(x * (sprint ? SprintSpeed : Speed), rb.velocity.y, y * (sprint ? SprintSpeed : Speed));
-    
-        bool isGrounded = true;//Physics.OverlapBox(GroundCheck.position, GroundCheckSize, Quaternion.identity, GroundMask).Length != 0;
-        if (jump && isGrounded)
+        if (IsGrounded())
+        {
+            current_nb_jump = 0;
+        }
+
+        if (jump && current_nb_jump < MAX_NB_JUMP)
         {
             rb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
             jump = false;
+            current_nb_jump++;
         }
+        else if (current_nb_jump == MAX_NB_JUMP){ jump = false; }
     }
 }
